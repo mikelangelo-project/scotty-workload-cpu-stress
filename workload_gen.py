57182dbc5655afdeeea582544aa76e1b5846e6f3
Peter Chronz <peter.chronz@gmail.com>
2017-12-07 17:55:42 +0100

    Add logging output of resources and endpoint

--- workload_gen.py
+++ workload_gen.py
@@ -4,14 +4,17 @@ from scotty import utils
 
 logger = logging.getLogger(__name__)
 
+
 def run(context):
     workload = context.v1.workload
     exp_helper = utils.ExperimentHelper(context)
-#    my_resource = exp_helper.get_resource(workload.resources['my_resource']
+    my_resource = exp_helper.get_resource(workload.resources['cpu_stressor_resource'])
+    logger.info(my_resource)
     logger.info('{}'.format(workload.params['greeting']))
     logger.info('I\'m workload generator {}'.format(workload.name))
-#    logger.info('The resource endpoint is {}'.format(my_resource))
-    return result_description
+    logger.info('The resource endpoint is {}'.format(my_resource))
+    return 42
+
 
 def clean(context):
     pass
